85a2ac9aaaf2b5126471da58fc3104db720b4398
csric <2905491763@qq.com>
2023-10-09 14:24:35 -0400

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Channel-Independent-MIDI-CC-for-Arturia-Keystep-37
-A MIDI script for FL Studio, whose omni mode is strange. This code maps all channels' MIDI CC (not notes) to channel 1.
+# Channel Remap for Arturia Keystep 37
+A MIDI script for FL Studio, whose omni mode is strange. This code maps all MIDI CC, Pitch Bend and Aftertouch's channel number to channel 1.   
+Also applied better transport controls by [this repository](https://github.com/abbydiode/fl-studio-arturia-keystep-37/tree/main).
+![arturia_keystep_37](https://github.com/Cracking-Sciences/Channel-Independent-MIDI-CC-for-Arturia-Keystep-37/assets/59389055/9c217627-e378-4fcb-8476-b73c9a194260)
+
+## Installation
+- Copy python scripts to %USERPROFILE%/Documents/Image-Line/FL Studio/Settings/Hardware/%NEWFOLDER%
+- FL Studio -> Options -> MIDI Settings -> Controller type -> Arturia Keystep 37 Channel Map
+
+## Bonus
+Arturia keystep 37 Control Surface Preset, Arturia.fst:
+![Keystep 37 control surface](https://github.com/Cracking-Sciences/Channel-Remap-for-Arturia-Keystep-37/assets/59389055/5161cebd-c760-4dbb-8f72-804b324c0ccf)
+Use it in Patcher : )
---
+++ device_keystep_37_midi_cc_map.py
@@ -0,0 +1,65 @@
+#name=Arturia Keystep 37 Channel Map
+
+'''
+Presented by Cracking Sciences 
+'''
+
+import transport
+
+
+
+def event_print(event):
+    print(f"handled {event.handled},\
+        timestamp {event.timestamp},\
+        status {event.status},\
+        data1 {event.data1},\
+        data2 {event.data2},\
+        port {event.port},\
+        note {event.note},\
+        velocity {event.velocity},\
+        pressure {event.pressure},\
+        progNum {event.progNum},\
+        controlNum {event.controlNum},\
+        controlVal {event.controlVal},\
+        midiId {event.midiId},\
+        midiChan {event.midiChan}")
+
+BUTTON_RECORD = 0x32
+BUTTON_STOP = 0x33
+BUTTON_PLAY = 0x36
+
+def OnControlChange(event):
+    event.handled = False
+    if event.data1 == BUTTON_RECORD:
+        # print(f'{"Disabled" if transport.isRecording() else "Enabled"} recording')
+        transport.record()
+        event.handled = True
+    elif event.data1 == BUTTON_STOP and event.data2 > 0:
+        # print('Stopped playback')
+        transport.stop()
+        event.handled = True
+    elif event.data1 == BUTTON_PLAY and event.data2 > 0:
+        # print(f'{"Paused" if transport.isPlaying() else "Started"} playback')
+        transport.start()
+        event.handled = True
+    else:
+        # Mod Strip and Knobs
+        event.status -= event.midiChan
+        event.midiChan = 0
+    event_print(event)
+
+def OnPitchBend(event):
+    # Pitch Strip
+    event.handled = False
+    event.status -= event.midiChan
+    event.midiChan = 0
+    event_print(event)
+
+def OnChannelPressure(event):
+    # Channel Aftertouch
+    event.handled = False
+    event.status -= event.midiChan
+    event.midiChan = 0
+    event_print(event)
+
+# csric | 2023
